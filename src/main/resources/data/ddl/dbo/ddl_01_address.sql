412dd82483cf0cf049cacb0bdf42004fb2bc729b
sarika-kondakindi <147028875+sarika-kondakindi@users.noreply.github.com>
2023-10-26 13:45:53 +1100

    ELDHLP-67: Rename columns to follow column structure

--- src/main/resources/data/ddl/dbo/ddl_01_address.sql
+++ src/main/resources/data/ddl/dbo/ddl_01_address.sql
@@ -10,7 +10,9 @@
  * 
  *  Date -------  JIRA  ---- Author ----------- Comments
  *  17/10/2023    ELDHLP-67  Sarika Kondakindi  Entity to store the permanent and mailing address details of users. 
- * 
+ *  26/10/2023    ELDHLP-67  Sarika Kondakindi  Rename columns mail_address_street_type,mail_address_suburb,
+                                                mail_address_state, mail_address_zip to follow column structure mail_adr_.
+ 
  */
 
 create table if not exists dbo.address(address_id               bigint,
@@ -25,10 +27,10 @@ create table if not exists dbo.address(address_id               bigint,
                                        mail_adr_unit_level_no   numeric(5),
                                        mail_adr_street_no       int,
                                        mail_adr_street_name     varchar(50) not null,
-                                       mail_address_street_type varchar(50) not null,
-                                       mail_address_suburb      varchar(50) not null,
-                                       mail_address_state       varchar(50) not null,
-                                       mail_address_zip         varchar(5) not null,
+                                       mail_adr_street_type varchar(50) not null,
+                                       mail_adr_suburb      varchar(50) not null,
+                                       mail_adr_state       varchar(50) not null,
+                                       mail_adr_zip         varchar(5) not null,
                                        mail_adr_country         varchar(30) not null,
                                        inserted_by              varchar(50) default current_user,
                                        inserted_date            timestamptz default now(),
