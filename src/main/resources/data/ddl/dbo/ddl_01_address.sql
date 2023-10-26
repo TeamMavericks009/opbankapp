afa0e2240a9f4a61b4ce5d9a95ed3dfb4c9c743e
sarika-kondakindi <147028875+sarika-kondakindi@users.noreply.github.com>
2023-10-26 14:04:55 +1100

    ELDHLP-67: address entity renamed columns to _addr_ instead of _adr_

--- src/main/resources/data/ddl/dbo/ddl_01_address.sql
+++ src/main/resources/data/ddl/dbo/ddl_01_address.sql
@@ -10,28 +10,27 @@
  * 
  *  Date -------  JIRA  ---- Author ----------- Comments
  *  17/10/2023    ELDHLP-67  Sarika Kondakindi  Entity to store the permanent and mailing address details of users. 
- *  26/10/2023    ELDHLP-67  Sarika Kondakindi  Rename columns mail_address_street_type,mail_address_suburb,
-                                                mail_address_state, mail_address_zip to follow column structure mail_adr_.
+ *  26/10/2023    ELDHLP-67  Sarika Kondakindi  Rename columns to follow column structure _addr_.
  
  */
 
 create table if not exists dbo.address(address_id               bigint,
-                                       perm_adr_unit_level_no   numeric(5),
-                                       perm_adr_street_no       int,
-                                       perm_adr_street_name     varchar(50) not null,
-                                       perm_adr_street_type     varchar(50) not null,
-                                       perm_adr_suburb          varchar(50) not null,
-                                       perm_adr_state           varchar(50) not null,
-                                       perm_adr_zip             varchar(5) not null,
-                                       perm_adr_country         varchar(30) not null,
-                                       mail_adr_unit_level_no   numeric(5),
-                                       mail_adr_street_no       int,
-                                       mail_adr_street_name     varchar(50) not null,
-                                       mail_adr_street_type varchar(50) not null,
-                                       mail_adr_suburb      varchar(50) not null,
-                                       mail_adr_state       varchar(50) not null,
-                                       mail_adr_zip         varchar(5) not null,
-                                       mail_adr_country         varchar(30) not null,
+                                       perm_addr_unit_level_no   numeric(5),
+                                       perm_addr_street_no       int,
+                                       perm_addr_street_name     varchar(50) not null,
+                                       perm_addr_street_type     varchar(50) not null,
+                                       perm_addr_suburb          varchar(50) not null,
+                                       perm_addr_state           varchar(50) not null,
+                                       perm_addr_zip             varchar(5) not null,
+                                       perm_addr_country         varchar(30) not null,
+                                       mail_addr_unit_level_no   numeric(5),
+                                       mail_addr_street_no       int,
+                                       mail_addr_street_name     varchar(50) not null,
+                                       mail_addr_street_type     varchar(50) not null,
+                                       mail_addr_suburb          varchar(50) not null,
+                                       mail_addr_state           varchar(50) not null,
+                                       mail_addr_zip             varchar(5) not null,
+                                       mail_addr_country         varchar(30) not null,
                                        inserted_by              varchar(50) default current_user,
                                        inserted_date            timestamptz default now(),
                                        updated_by               varchar(50),
